bf5e3462a40f21b4a4c2edba96d5fac305093ba0
Charles <cll258@cornell.edu>
2026-02-13 14:40:35 -0500

    mane

--- src/parser/ast.rs
+++ src/parser/ast.rs
@@ -18,27 +18,27 @@ pub enum Definition { // a definition is a method or a global declaration
 
 pub struct Method { // a method is a function, with a name, parameters, return types, and a block
     pub id: String,
-    pub declarations: <Vec<Declaration>>, // a parameter in a function..., no initialization
-    pub return_types: <Vec<Type>>,
+    pub declarations: Vec<Declaration>, // a parameter in a function..., no initialization
+    pub return_tys: Vec<Type>,
     pub block: Block,
 }
 
 pub struct Declaration {
     pub id: String,
-    pub type: Type,
+    pub ty: Type,
 }
 
 // i think this can also be applied to declarations within a method, which are 'global' in that scope
 pub struct GlobalDeclaration {
     pub id: String,
-    pub type: Type,
-    pub init: Optional<Expr>,
+    pub ty: Type,
+    pub init: Option<Expr>,
 }
 
 pub enum Type {
     Bool,
     Int,
-    Array(Box<Type>), // pretty sure this should only really be Box<Int>...
+    Array(Box<Type>),
 }
 
 pub struct Block {
@@ -49,22 +49,14 @@ pub enum Stmt {
     Expression(Expr),
     Declaration(Declaration),
     Assignment(Assignment),
-    If {expr: Expr, else: Optional<Else>}, // can optionally be chained with an else
-    While(While), // should just contain an expression i think...
+    If {cond: Expr, then_block: Block, else: Option<Block>}, // can Optionally be chained with an else
+    While {cond: Expr, body: Block}, // should just contain an expression i think...
     Return(Vec<Expr>), // list of expressions
-    Proc {id: String, args: Vec<Arg>} , // name and args i believe
-}
-
-// a single argument passed to a function can be a literal, an identifier, or an expression
-pub enum Arg {
-    Literal(Literal),
-    Identifier(String),
-    Expr(Expr)
+    Proc {id: String, args: Vec<Expr>} , // name and args i believe
 }
 
 // assignment can be a list of expressions, procedure calls, or types...
-// tf ...
-pub enum Op {
+pub enum BinaryOp {
     Add,
     Sub,
     Mul,
@@ -78,6 +70,19 @@ pub enum Op {
     Gte,
 }
 
+// not sure how we wanna represent strings but fine rn...
+pub enum Literal {
+    Int(i64),
+    Bool(bool),
+    Char(char),
+    String(String),
+}
+
+pub enum UnaryOp {
+    Neg,
+    Not,
+}
+
 // expressions can exist after an equals sign, after an if / while statement...
 pub enum Expr {
     Literal(Literal),
@@ -85,11 +90,11 @@ pub enum Expr {
     Grouped(Box<Expr>), // ?
     Binary {
         left: Box<Expr>,
-        op: Op,
+        op: BinaryOp,
         right: Box<Expr>,
     },
     Unary {
-        op: Op,
+        op: UnaryOp,
         right: Box<Expr>,
     },
 }
