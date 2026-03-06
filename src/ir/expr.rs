4ae42f3a9c5c96953b6be2d5c404d4b71fe65bbb
Fadi Ismail <fmi4@cornell.edu>
2026-03-06 10:19:18 -0500

    Add core IR node definitions and operator/symbol types for expressions and statements.

---
+++ src/ir/expr.rs
@@ -0,0 +1,35 @@
+use super::op::{BinOp, UnOp};
+use super::stmt::Stmt;
+use super::symbol::{Label, Temp};
+
+#[derive(Debug, Clone, PartialEq, Eq)]
+pub enum Expr {
+    // literal
+    Const(i64),
+    // Variable
+    Temp(Temp),
+    // Logical Operations
+    BinOp {
+        op: BinOp,
+        left: Box<Expr>,
+        right: Box<Expr>,
+    },
+    UnOp {
+        op: UnOp,
+        expr: Box<Expr>,
+    },
+    // Content of Memory Address
+    Mem(Box<Expr>),
+    // Function Call (target: func, args: parameters)
+    Call {
+        target: Box<Expr>,
+        args: Vec<Expr>,
+    },
+    // Address of label
+    Name(Label),
+    // (s ; e)
+    ESeq {
+        stmt: Box<Stmt>,
+        expr: Box<Expr>,
+    },
+}
---
+++ src/ir/mod.rs
@@ -0,0 +1,12 @@
+mod symbol;
+mod op;
+mod expr;
+mod stmt;
+mod pretty;
+
+//public API
+pub use symbol::*;
+pub use op::*;
+pub use expr::*;
+pub use stmt::*;
+pub use pretty::*;
